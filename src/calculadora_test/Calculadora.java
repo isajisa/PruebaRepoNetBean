1d30d015732efdb4ac28e92d83a9488a68a2f86b
isajisa <isabel.j.s.2004@gmail.com>
2025-05-07 20:31:32 +0200

    Subida del proyecto

---
+++ src/calculadora_test/Calculadora.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora_test;
+
+/**
+ *
+ * @author isajisa
+ */
+public class Calculadora {
+    public static void main(String[] args) {
+        
+    }
+    private int suma=0;
+    
+    public int suma(int a, int b){
+        suma = a + b;
+        return suma;
+    }
+}
